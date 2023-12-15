fc80a7d10c9f6a7c5f10a57ce91701b8e8318ed2
Yiğit Kaaan Koçak <kaanyigitkocak@gmail.com>
2023-12-15 20:31:57 +0300

    report.cs bug fix

--- src/intTechBeta/Domain/Entities/Report.cs
+++ src/intTechBeta/Domain/Entities/Report.cs
@@ -5,8 +5,6 @@ using System.Text.Json.Serialization;
 
 public class Report : Entity<Guid>
 {
-
-    public Guid Id { get; set; }
     public string Title { get; set; }
     public string Link { get; set; }
     public string Description { get; set; }
